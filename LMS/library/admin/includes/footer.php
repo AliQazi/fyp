1ea4734230687eb3259be39f87d3929cd97f96be
AliQazi <56753079+AliQazi@users.noreply.github.com>
2023-01-15 23:20:05 +0500

    Add files via upload
    final year project with documentation added

---
+++ LMS/library/admin/includes/footer.php
@@ -0,0 +1,11 @@
+<section class="footer-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                &copy;Online Library Management System | Designed by
+                    : Muhammad Ali Qazi</a>
+            </div>
+
+        </div>
+    </div>
+</section>
